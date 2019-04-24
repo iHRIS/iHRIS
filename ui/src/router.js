953e883cea5fc255ffea0e3daa4b6918cafb08e3
Brian Lamb <fusani.applications@gmail.com>
2019-04-24 13:28:39 -0400

    Use router for pages, add stubs for documentation

--- ui/src/router.js
+++ ui/src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -11,7 +10,8 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () =>
+        import(/* webpackChunkName: "home" */ "./views/Home.vue")
     },
     {
       path: "/about",
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/documentation",
+      name: "documentation",
+      component: () =>
+        import(/* webpackChunkName: "documentation" */ "./views/Documentation.vue")
     }
   ]
 });
