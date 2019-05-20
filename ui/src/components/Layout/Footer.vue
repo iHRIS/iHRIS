3812595ba066c84e110090aa249ec2f4a8c828ca
Brian Lamb <fusani.applications@gmail.com>
2019-05-20 16:14:10 -0400

    Fix capitalization on footer links

--- ui/src/components/Layout/Footer.vue
+++ ui/src/components/Layout/Footer.vue
@@ -3,16 +3,16 @@
     <v-layout align-center>
       <v-spacer />
       <v-flex xs4>
-        <v-btn to="user-manual" flat>
+        <v-btn to="user-manual" flat class="text-capitalize">
           User Manual
         </v-btn>
-        <v-btn to="feedback" flat>
+        <v-btn to="feedback" flat class="text-capitalize">
           Feedback
         </v-btn>
-        <v-btn href="https://www.ihris.org" target="_blank" flat>
+        <v-btn href="https://www.ihris.org" target="_blank" flat class="text-none">
           iHRIS Website
         </v-btn>
-        <v-btn to="terms-and-conditions" flat>
+        <v-btn to="terms-and-conditions" flat class="text-capitalize">
           Terms &amp; Conditions
         </v-btn>
       </v-flex>
