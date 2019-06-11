498a01abbda3b8ce87bc5f27a6b4d739f19b2d3a
Brian Lamb <fusani.applications@gmail.com>
2019-06-11 13:43:09 -0400

    Describe page and get fields from structure definition

--- server/routes/practitioner.js
+++ server/routes/practitioner.js
@@ -3,20 +3,40 @@ const models = require("../models");
 var router = express.Router();
 var axios = require("axios");
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env]["fhir"];
+const config = require(__dirname + '/../config/config.json')[env];
 
-router.get("/describe", function(req, res, next) {
-  axios.get(config.server + "/fhir/StructureDefinition/Practitioner", {
+router.get("/describe/page", function(req, res, next) {
+  let practitionerPage = config.definitions.practitionerPage;
+
+  if (!practitionerPage) {
+    return res.status(400).json("No practitioner page definition found.");
+  }
+
+  axios.get(config.fhir.server + "/fhir/" + practitionerPage, {
     params: {},
     withCredentials: true,
     auth: {
-      username: config.username,
-      password: config.password
+      username: config.fhir.username,
+      password: config.fhir.password
     }
   }).then(response => {
     res.status(201).json(response.data);
   }).catch(err => {
-    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.get("/describe/definition/:definition", function(req, res, next) {
+  axios.get(config.fhir.server + "/fhir/StructureDefinition/" + req.params.definition, {
+    params: {},
+    withCredentials: true,
+    auth: {
+      username: config.fhir.username,
+      password: config.fhir.password
+    }
+  }).then(response => {
+    res.status(201).json(response.data);
+  }).catch(err => {
     res.status(400).json(err);
   });
 });
