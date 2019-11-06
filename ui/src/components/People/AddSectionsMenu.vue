0257c9cc781b83c7c283f1cb45487edaae39faad
Brian Lamb <fusani.applications@gmail.com>
2019-11-06 10:33:28 -0500

    Use v-list-items for add sections menu

--- ui/src/components/People/AddSectionsMenu.vue
+++ ui/src/components/People/AddSectionsMenu.vue
@@ -1,31 +1,31 @@
 <template>
   <v-card class="primary darken-1 white--text">
     <v-card-title class="display-1">Add Sections</v-card-title>
-    <v-list class="primary darken-1 white--text">
-      <v-list-group
+    <v-card-text>
+      <v-list
+        class="primary darken-1 white--text"
         v-for="item in menu"
         :key="item.title"
-        prepend-icon="add"
-        no-action
-        class="pb-3"
       >
-        <template v-slot:activator>
-          <v-list-tile
-            active-class="primary darken-2"
-            @click.stop="showForm(item.title, item.definition)"
-          >
-            <v-list-tile-content>
-              <v-list-tile-title class="text-uppercase font-weight-bold">
-                {{ item.title | sentenceCase }}
-              </v-list-tile-title>
-              <v-list-tile-sub-title class="white--text">
-                {{ item.subtitle }}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list-group>
-    </v-list>
+        <v-list-item
+          active-class="primary darken-2"
+          @click.stop="showForm(item.title, item.definition)"
+          three-line
+        >
+          <v-list-item-icon>
+            <v-icon class="white--text">mdi-plus</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text text-uppercase font-weight-bold">
+              {{ item.title | sentenceCase }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="white--text">
+              {{ item.subtitle }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 
