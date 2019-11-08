c7325f1ab5d6c03b47cb69c9d43d5afe52a8f487
Brian Lamb <fusani.applications@gmail.com>
2019-11-08 13:51:29 -0500

    Check for valid login credentials

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
   <v-container>
+    <Alert ref="alert" />
+
     Dashboard
   </v-container>
 </template>
+
+<script>
+import Alert from "@/components/Layout/Alert.vue";
+
+export default {
+  components: {
+    Alert
+  },
+  mounted() {
+    if (this.login) {
+      this.$refs.alert.changeMessage("Login successful", "success");
+    }
+  },
+  props: {
+    login: {
+      default: false,
+      type: Boolean
+    }
+  }
+};
+</script>
