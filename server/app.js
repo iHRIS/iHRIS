64cec96ec0b48e8877eaed30d8d4b42cca832f10
Brian Lamb <fusani.applications@gmail.com>
2019-11-14 16:55:59 -0500

    Render dashboard titles on home page

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var dashboardRouter = require('./routes/dashboard');
 var indexRouter = require('./routes/index');
 var practitionerRouter = require('./routes/practitioner');
 var relationshipRouter = require('./routes/relationship');
@@ -23,6 +24,7 @@ app.use( function( req, res, next ) {
 })
 
 app.use('/', indexRouter);
+app.use('/dashboard', dashboardRouter);
 app.use('/practitioner', practitionerRouter);
 app.use('/relationship', relationshipRouter);
 app.use('/user', userRouter);
