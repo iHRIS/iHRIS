2e13317488ccf2475ff67919f1d2c631ad7fa06c
Luke Duncan <lduncan@intrahealth.org>
2020-09-03 06:53:58 -0600

    Fixed issue with catching error on questionnaire submission.

--- ihris-backend/routes/questionnaire.js
+++ ihris-backend/routes/questionnaire.js
@@ -45,9 +45,9 @@ router.post("/QuestionnaireResponse", (req, res, next) => {
           fhirAxios.create( bundle ).then ( (results) => {
             next()
           } ).catch( (err) => {
-            winston.error(err)
-            winston.error(JSON.stringify(err.response.data,null,2))
-            return res.status( err.response.status ).json( err.response.data )
+            winston.error(err.message)
+            //return res.status( err.response.status ).json( err.response.data )
+            return res.status( 400 ).json( { err: err.message } )
           } )
 
         } ).catch( (err) => {
