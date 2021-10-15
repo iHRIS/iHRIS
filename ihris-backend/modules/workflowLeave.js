1d022190817f29197d97ff8d3b38e50567186c7a
Nobert Mijumbi <nobertmn@gmail.com>
2021-10-15 10:34:24 +0300

    Added Error handling to Performance and Leave

--- ihris-backend/modules/workflowLeave.js
+++ ihris-backend/modules/workflowLeave.js
@@ -88,13 +88,48 @@ const workflowLeave = {
           } )
           resolve( bundle )
         } else {
-          reject("Invalid input")
+          resolve(await workflowLeave.outcome("No Leave Type provided"))
         }
       } ).catch( (err) => {
         logger.error(err)
-        reject(err)
+        resolve(await workflowLeave.outcome("No Person(Practitioner) Record found"))
       } )
     } )
+  },
+  postProcess: ( req, results ) => {
+    return new Promise( (resolve, reject) => {
+        if ( results.entry && results.entry.length > 0 && results.entry[0].response.location ) {
+          if ( !req.body.meta ) req.body.meta = {}
+          if ( !req.body.meta.tag ) req.body.meta.tag = []
+          req.body.meta.tag.push( { system: "http://ihris.org/fhir/tags/resource", code: results.entry[0].response.location } )
+          resolve( req )
+        }
+    })
+  },
+  outcome: (message) => {
+    return new Promise ((resolve, reject ) => {
+      let outcomeBundle = {
+        resourceType: "Bundle",
+        type: "transaction",
+        entry: [{
+          resource:{
+            resourceType: "OperationOutcome",
+            issue: [
+            {
+              severity: "error",
+              code: "exception",
+              diagnostics: message
+            }]
+          },
+          request: {
+            method: "POST",
+            url: "OperationOutcome"
+          }
+        }]
+      }
+      winston.info(JSON.stringify(outcomeBundle,null,2))
+      resolve(outcomeBundle)
+    })
   }
 }
 
--- ihris-backend/modules/workflowPerformance.js
+++ ihris-backend/modules/workflowPerformance.js
@@ -77,13 +77,48 @@ const workflowPerformance = {
           } )
           resolve( bundle )
         } else {
-          reject("Invalid input")
+          resolve(await workflowPerformance.outcome("No Evaluator provided"))
         }
       } ).catch( (err) => {
         logger.error(err)
-        reject(err)
+        resolve(await workflowPerformance.outcome("No Person(Practitioner) Record found"))
       } )
     } )
+  },
+  postProcess: ( req, results ) => {
+    return new Promise( (resolve, reject) => {
+        if ( results.entry && results.entry.length > 0 && results.entry[0].response.location ) {
+          if ( !req.body.meta ) req.body.meta = {}
+          if ( !req.body.meta.tag ) req.body.meta.tag = []
+          req.body.meta.tag.push( { system: "http://ihris.org/fhir/tags/resource", code: results.entry[0].response.location } )
+          resolve( req )
+        }
+    })
+  },
+  outcome: (message) => {
+    return new Promise ((resolve, reject ) => {
+      let outcomeBundle = {
+        resourceType: "Bundle",
+        type: "transaction",
+        entry: [{
+          resource:{
+            resourceType: "OperationOutcome",
+            issue: [
+            {
+              severity: "error",
+              code: "exception",
+              diagnostics: message
+            }]
+          },
+          request: {
+            method: "POST",
+            url: "OperationOutcome"
+          }
+        }]
+      }
+      winston.info(JSON.stringify(outcomeBundle,null,2))
+      resolve(outcomeBundle)
+    })
   }
 }
 
