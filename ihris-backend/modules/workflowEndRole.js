44dd8352765f8298119e124c2a655273c124e5b3
pierrekasongo88@gmail.com <pierrekasongo88@gmail.com>
2022-01-19 17:15:16 +0200

    Fixing End job bugs

--- ihris-backend/modules/workflowEndRole.js
+++ ihris-backend/modules/workflowEndRole.js
@@ -12,15 +12,13 @@ const workflowEndRole = {
       }
       //winston.info(JSON.stringify( req.body,null,2))
       let resource = await fhirAxios.read( "PractitionerRole", req.query.practitionerrole )
-    
       try {
         if (resource){
-          
           if ( req.body && req.body.item 
             && req.body.item && req.body.item[0].linkId === "PractitionerRole"
             && req.body.item[0].item && req.body.item[0].item[0].linkId === "period.end" 
             && req.body.item[0].item[0].answer && req.body.item[0].item[0].answer[0] 
-            && req.body.item[0].item[0].answer[0].valueDateTime
+            && req.body.item[0].item[0].answer[0].valueDate
             && req.body.item[0].item[1].linkId === "departure" 
             && req.body.item[0].item[1].answer && req.body.item[0].item[1].answer[0] 
             && req.body.item[0].item[1].answer[0].valueCoding ) {
@@ -30,7 +28,7 @@ const workflowEndRole = {
               let positionStatus = {url:"http://ihris.org/fhir/StructureDefinition/ihris-practitionerrole-position-status",
                           valueCoding: req.body.item[0].item[2].answer[0].valueCoding }
               resource.extension.push(positionStatus)
-              resource.period.end = req.body.item[0].item[0].answer[0].valueDateTime
+              resource.period.end = req.body.item[0].item[0].answer[0].valueDate
               resource.active = false
          
               bundle.entry.push( { 
@@ -40,15 +38,27 @@ const workflowEndRole = {
                   url: resource.resourceType +"/"+ resource.id
                 }
               } )
-            if ( resource.practitioner && resource.practitioner.reference ) {
-              req.body.subject = { reference: resource.practitioner.reference }
+              //console.log("Resource ",resource.practitioner.reference)
+              if ( resource.practitioner && resource.practitioner.reference ) {
+                req.body.subject = { reference: resource.practitioner.reference }
+              }
+              let practitioner = await fhirAxios.read(
+                "Practitioner",
+                resource.practitioner.reference.replace("Practitioner/", "")
+              );
+              practitioner.active = false;
+              bundle.entry.push({
+                resource: practitioner,
+                request: {
+                  method: "PUT",
+                  url: practitioner.resourceType + "/" + practitioner.id,
+                },
+              });
+              resolve( bundle )
+            } else {
+              winston.info("No Position End Date or Reason For Depature")
+              resolve(await workflowEndRole.outcome("Position End date or Reason for Depature not provided"))
             }
-            winston.info(JSON.stringify(bundle,null,2))
-            resolve( bundle )
-          } else {
-            winston.info("No Position End Date or Reason For Depature")
-            resolve(await workflowEndRole.outcome("Position End date or Reason for Depature not provided"))
-          }
         } else{
           winston.info("No Position")
           resolve(await workflowEndRole.outcome("No Position Returned"))
