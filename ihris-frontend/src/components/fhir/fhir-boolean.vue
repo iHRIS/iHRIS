de8a2735bd75ebae802408fcdad04afc94dc5087
Nobert Mijumbi <nobertmn@gmail.com>
2022-11-01 12:42:38 +0300

    Fixed more frontend translation issues

--- ihris-frontend/src/components/fhir/fhir-boolean.vue
+++ ihris-frontend/src/components/fhir/fhir-boolean.vue
@@ -10,7 +10,7 @@
         :error-messages="errors"
         @change="errors = []"
       >
-      <template #label>{{$t(`App.fhir-resources-texts.${display}`)}}: {{$t(`App.fhir-resources-texts.${value.toString()}`)}} <span v-if="required" class="red--text font-weight-bold">*</span></template>
+      <template #label>{{$t(`App.fhir-resources-texts.${display}`)}}: {{value.toString()}} <span v-if="required" class="red--text font-weight-bold">*</span></template>
       </v-switch>
     </template>
     <template #header>
