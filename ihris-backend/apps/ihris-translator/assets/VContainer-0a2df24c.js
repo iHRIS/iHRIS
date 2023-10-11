5185a947fee0d58eb56b769f4e74b3d85190e746
Ally Shaban <allyshaban5@gmail.com>
2023-10-11 14:19:08 +0300

    making dashboard and translation apps available by default

---
+++ ihris-backend/apps/ihris-translator/assets/VContainer-0a2df24c.js
@@ -0,0 +1,1 @@
+import{p as n,ak as o,al as r,k as l,am as c,z as i,e as m}from"./index-ecdb1354.js";const u=n({fluid:{type:Boolean,default:!1},...o(),...r()},"v-container"),d=l()({name:"VContainer",props:u(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=c();return i(()=>m(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{d as V};
