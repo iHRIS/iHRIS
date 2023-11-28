49f6b49f37953bb692c8a4ecb8b77e3d1d75a252
Ally Shaban <allyshaban5@gmail.com>
2023-11-28 07:36:37 +0300

    adding text display in questionnare fields with long texts

--- ihris-frontend/src/components/fhir/fhir-string.vue
+++ ihris-frontend/src/components/fhir/fhir-string.vue
@@ -1,7 +1,8 @@
 <template>
   <ihris-element :edit="edit" :loading="false" v-if="!hide">
     <template #form>
-      <v-text-field 
+      <v-textarea
+        v-if="displayType === 'text'"
         :error-messages="errors" 
         @change="errors = []" 
         :disabled="disabled"
@@ -15,7 +16,24 @@
         @click:append="showPassword = !showPassword"
         dense
       >
-      <template #label>{{$t(`App.fhir-resources-texts.${display}`)}}<span v-if="required" class="red--text font-weight-bold">*</span></template>
+        <template #label>{{$t(`App.fhir-resources-texts.${display}`)}}<span v-if="required" class="red--text font-weight-bold">*</span></template>
+      </v-textarea>
+      <v-text-field
+        v-else
+        :error-messages="errors"
+        @change="errors = []" 
+        :disabled="disabled"
+        :label="$t(`App.fhir-resources-texts.${display}`)"
+        v-model="value" 
+        outlined 
+        hide-details="auto" 
+        :rules="rules" 
+        :type="isPassword ? (showPassword ? 'text' : 'password') : 'text'"
+        :append-icon="isPassword ? (showPassword ? 'mdi-eye' : 'mdi-eye-off') : ''"
+        @click:append="showPassword = !showPassword"
+        dense
+      >
+        <template #label>{{$t(`App.fhir-resources-texts.${display}`)}}<span v-if="required" class="red--text font-weight-bold">*</span></template>
       </v-text-field>
     </template>
     <template #header>
